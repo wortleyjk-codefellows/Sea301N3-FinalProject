39f488e8e4da9fa7bd58e9cd7984b86a0bdd7c9b
geoffreycc <gcox3d@gmail.com>
2016-07-17 09:48:33 -0700

    Removed public from script tag paths.

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-var path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');

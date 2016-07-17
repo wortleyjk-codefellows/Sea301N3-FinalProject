43fc11b2c2cfea8daf1be3886c04e371927e1ef8
geoffreycc <gcox3d@gmail.com>
2016-07-17 09:33:36 -0700

    Added public folder and updated server.js file.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');

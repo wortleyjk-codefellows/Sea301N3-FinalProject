824b7d94676e8e01c3d62214a870b654b9495b35
geoffreycc <gcox3d@gmail.com>
2016-07-17 14:17:14 -0700

    Working on google maps stuff.

--- public/scripts/vetControl.js
+++ public/scripts/vetControl.js
@@ -1,5 +1,44 @@
 (function(module) {
-  var vet = {};
+  var vets = {};
 
-  module.vet = vet;
+  vets.initMap = function() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644}, // Use zipcode from input field
+      zoom: 10 // zoom level 10 is city
+    });
+  };
+
+
+  module.vets = vets;
 })(window);
+
+// var map;
+// var service;
+// var infowindow;
+//
+// function initialize() {
+//   var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+//
+//   map = new google.maps.Map(document.getElementById('map'), {
+//       center: pyrmont,
+//       zoom: 15
+//     });
+//
+//   var request = {
+//     location: pyrmont,
+//     radius: '500',
+//     query: 'restaurant'
+//   };
+//
+//   service = new google.maps.places.PlacesService(map);
+//   service.textSearch(request, callback);
+// }
+//
+// function callback(results, status) {
+//   if (status == google.maps.places.PlacesServiceStatus.OK) {
+//     for (var i = 0; i < results.length; i++) {
+//       var place = results[i];
+//       createMarker(results[i]);
+//     }
+//   }
+// }

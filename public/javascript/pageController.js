fb14f4501b79c967e202452610acdcdd7ce4e272
James Wortley <jameswortley@James-Wortleys-MacBook-Pro.local>
2016-07-17 11:35:51 -0700

    modified first page view

--- public/javascript/pageController.js
+++ public/javascript/pageController.js
@@ -1,5 +1,7 @@
 (function(module){
   var controller = {};
   $('section').hide(); //hides all initial sections first.
+  $('#stage-1').show();
+  $('#nav-selection').slideUp(400);
   module.controller = controller
 })(window)

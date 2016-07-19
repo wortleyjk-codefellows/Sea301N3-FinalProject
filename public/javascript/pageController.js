31c266ede2c20e94c3938bfe5ca5ca7fd2115c14
geoffreycc <gcox3d@gmail.com>
2016-07-18 20:00:05 -0700

    Made method to display the full details of a pet.

--- public/javascript/pageController.js
+++ public/javascript/pageController.js
@@ -3,5 +3,13 @@
   // $('section').hide(); //hides all initial sections first.
   // $('#stage-1').show();
   $('#nav-selection').slideUp(400);
-  module.controller = controller
-})(window)
+  module.controller = controller;
+})(window);
+
+
+controller.displayFullPetDetails = function(pet) {
+  var fullDetailHtml = $('#petDetails').html();
+  var fullDetailTemplate = handlebars.compile(fullDetailHtml);
+  fullDetailTemplate(pet);
+  //Update the template in index.html ----- create some event on button that gets pet
+};

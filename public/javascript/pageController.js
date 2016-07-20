6416903a7fd157f1d4d2cccdfa0233c9052a9db1
James Wortley <jameswortley@James-Wortleys-MacBook-Pro.local>
2016-07-19 18:29:31 -0700

    finished handlebars template for results

--- public/javascript/pageController.js
+++ public/javascript/pageController.js
@@ -1,7 +1,37 @@
 (function(module){
   var controller = {};
-  // $('section').hide(); //hides all initial sections first.
-  // $('#stage-1').show();
-  $('#nav-selection').slideUp(400);
+  // $('.petButton').on('click',function(){
+  //   $('section').hide();
+  //   $('#searchSection').slideDown(400);
+  // });
+
+  // $('#find-new-pet-btn').on('click', function(){
+  //   $('section').hide();
+  //   $('#narrowResults').toggle();
+  //   alert('test')
+  // });
+
+  $('body').on('click', '.petButton', function(){
+    $('section').slideUp(400);
+    $('#searchSection').slideDown(400);
+  });
+
+  $('body').on('click', '#find-new-pet-btn', function(){
+    $('section').slideUp(400);
+    $('#totalMatches').slideDown(400);
+  });
+  $('body').on('click', '#search-btn-transition', function(){
+    $('section').slideUp(400);
+    $('#filterResults').slideDown(400);
+  });
+  $('body').on('click', '#show-me-btn', function(){
+    $('section').slideUp(400);
+    pets.displayMatches();
+    $('#narrowResults').slideDown(400);
+  });
+  //
+  $('section').hide(); //hides all initial sections first.
+  $('#stage-1').show();
+  $('#nav-selection').hide();
   module.controller = controller
 })(window)
